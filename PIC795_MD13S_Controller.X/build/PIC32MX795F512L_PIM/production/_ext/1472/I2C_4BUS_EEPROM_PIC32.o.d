7792c83ce15ae0d089b955707166675a78753a56
JavaJimbo <JimSedgwick66@gmail.com>
2022-03-04 21:14:05 -0500

    Add files via upload
    Added project files

---
+++ PIC795_MD13S_Controller.X/build/PIC32MX795F512L_PIM/production/_ext/1472/I2C_4BUS_EEPROM_PIC32.o.d
@@ -0,0 +1,7 @@
+build/PIC32MX795F512L_PIM/production/_ext/1472/I2C_4BUS_EEPROM_PIC32.o: \
+ ../I2C_4BUS_EEPROM_PIC32.c ../I2C_4BUS_EEPROM_PIC32.h \
+ ../GenericTypeDefs.h
+
+../I2C_4BUS_EEPROM_PIC32.h:
+
+../GenericTypeDefs.h:
---
+++ PIC795_MD13S_Controller.X/build/PIC32MX795F512L_PIM/production/_ext/1472/main.o.d
@@ -0,0 +1,10 @@
+build/PIC32MX795F512L_PIM/production/_ext/1472/main.o: ../main.c \
+ ../Delay.h ../Compiler.h ../I2C_4BUS_EEPROM_PIC32.h ../GenericTypeDefs.h
+
+../Delay.h:
+
+../Compiler.h:
+
+../I2C_4BUS_EEPROM_PIC32.h:
+
+../GenericTypeDefs.h:
